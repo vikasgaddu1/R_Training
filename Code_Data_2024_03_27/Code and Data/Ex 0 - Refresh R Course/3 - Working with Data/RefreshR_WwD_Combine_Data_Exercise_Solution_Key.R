d1d89f062eb219fe877fbb3a406c90e56a716cab
Vikas Gaddu <anovagroups@gmail.com>
2024-08-20 14:18:15 +0000

    Initial commit

--- Code_Data_2024_03_27/Code and Data/Ex 0 - Refresh R Course/3 - Working with Data/RefreshR_WwD_Combine_Data_Exercise_Solution_Key.R
+++ Code_Data_2024_03_27/Code and Data/Ex 0 - Refresh R Course/3 - Working with Data/RefreshR_WwD_Combine_Data_Exercise_Solution_Key.R
@@ -1,11 +1,31 @@
 # Anova Accel2R - Clinical R Training -----------------------------------
 # © 2021 Anova Groups All rights reserved
 
+# Create a tibble data frame named demo with the following variables and values:
+#   study: "Study B"
+# inv: 2000
+# patient: 1 and 2
+# sex: "M" and "F"
+# dob: "1986-01-11" and "1987-07-01"
+# Then, create another tibble data frame named demo_add with the following variables and values:
+#   study: "Study B"
+# inv: 2000
+# patient: 3
+# sex: "F"
+# dob: "1984-10-30"
+# Next, combine the rows of demo and demo_add into a new tibble data frame named demo_2.
+# Finally, create a vector trt with the values "Placebo", "Active", and NA, and bind 
+# it as a new column named ARM to demo_2 to create a new tibble data frame named demo_all.
+
 # Title: Solution Key
 
 library(tidyverse)
 
 # Exercise Step 2
+# Create a tibble data frame
+# Define a variable named study, inv, patient, sex and dob with values as follows
+# 
+
 # Code to build tibble data frames
 demo <-
   tribble(
